18ce4f12c7795688501ca39726264c67184d55e4
Victoria Huang <vctrah@gmail.com>
2018-01-09 00:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  var uppercase = string.toUpperCase();
+  console.log(uppercase);
+}
+
+function logWhisper(string) {
+  var lowercase = string.toLowerCase();
+  console.log(lowercase);
+}
