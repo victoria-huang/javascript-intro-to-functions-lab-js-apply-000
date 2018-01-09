ed957186d44636ab4e144b87ebba8339eaaa45f6
Victoria Huang <vctrah@gmail.com>
2018-01-09 00:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-  } 
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
